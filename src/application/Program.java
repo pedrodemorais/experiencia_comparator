79f8ad15b70ace60d3b981d212f1abfafc6acb96
Pedro de Morais <pedrodemorais1@gmail.com>
2021-03-24 21:46:18 -0300

    Atualizado para funcao anonima com funcao lambida

--- src/application/Program.java
+++ src/application/Program.java
@@ -18,14 +18,11 @@ public class Program {
 		lista.add(new Product("DVD",300.00));
 		lista.add(new Product("Fogao",500.00));
 		
-		Comparator<Product> comp= new Comparator<Product>() {
-
-			@Override
-			public int compare(Product p1, Product p2) {
-				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			}
+		Comparator<Product> comp= (p1,p2) -> {
+			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
 			
 		};
+	
 		
 		lista.sort(comp);
 		
