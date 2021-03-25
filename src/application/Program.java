5e2a322d5fada1fd8540a37bb85ac3eec233b54e
Pedro de Morais <pedrodemorais1@gmail.com>
2021-03-24 21:56:52 -0300

    Comparator expressao lambida direto no argumento

--- src/application/Program.java
+++ src/application/Program.java
@@ -11,24 +11,20 @@ public class Program {
 
 	public static void main(String[] args) {
 		Locale.setDefault(Locale.US);
-		
+
 		List<Product> lista = new ArrayList();
-		lista.add(new Product("TV",1900.00));
-		lista.add(new Product("Geladeira",2900.00));
-		lista.add(new Product("DVD",300.00));
-		lista.add(new Product("Fogao",500.00));
-		
-		Comparator<Product> comp= (p1,p2) -> {
-			return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
-			
-		};
-	
-		
-		lista.sort(comp);
-		
-		for(Product p: lista) {
+		lista.add(new Product("TV", 1900.00));
+		lista.add(new Product("Geladeira", 2900.00));
+		lista.add(new Product("DVD", 300.00));
+		lista.add(new Product("Fogao", 500.00));
+
+		//Comparator<Product> comp = (p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+
+		lista.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+		for (Product p : lista) {
 			System.out.println(p);
-			
+
 		}
 
 	}
