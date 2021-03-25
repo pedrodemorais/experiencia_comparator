1b1791b1adbe7148fabc1634d42a869bebadaadf
Pedro de Morais <pedrodemorais1@gmail.com>
2021-03-24 21:39:10 -0300

    Usando a sintaxe da Classe Anonima, desconsiderando a classe MyComparator e utilizando a implementacao na Classe principal

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Locale;
 
@@ -17,7 +18,16 @@ public class Program {
 		lista.add(new Product("DVD",300.00));
 		lista.add(new Product("Fogao",500.00));
 		
-		lista.sort(new MyComparator());
+		Comparator<Product> comp= new Comparator<Product>() {
+
+			@Override
+			public int compare(Product p1, Product p2) {
+				return p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase());
+			}
+			
+		};
+		
+		lista.sort(comp);
 		
 		for(Product p: lista) {
 			System.out.println(p);
